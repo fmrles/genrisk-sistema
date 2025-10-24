fee72835fc61e81534feb769514953924bc23772
Francisco <f.jesusmorales.m@gmail.com>
2025-10-24 00:35:43 -0300

    first commit

---
+++ src/main/java/com/genrisk/sistema/GenriskSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.genrisk.sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenriskSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenriskSistemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genrisk/sistema/GenriskSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genrisk.sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenriskSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
