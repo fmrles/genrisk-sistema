0467a26a8e5cae56c4c410c4f704ed29a190019f
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-10-31 13:03:48 -0300

    Repositorio de Paciente

---
+++ src/main/java/com/genrisk/sistema/repository/PacienteRepository.java
@@ -0,0 +1,9 @@
+package com.genrisk.sistema.repository;
+
+import com.genrisk.sistema.model.entity.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Integer> {
+}
