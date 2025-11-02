a97147ecae2b1f379af12508a7367c2e560dfffc
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-11-02 12:36:52 -0300

    Eliminación de Interfaces genéricas

--- src/main/java/com/genrisk/sistema/interfaces/Command.java
+++
@@ -1,7 +0,0 @@
-package com.genrisk.sistema.interfaces;
-
-import org.springframework.http.ResponseEntity;
-
-public interface Command <I, O>{
-    ResponseEntity<O> execute (I input);
-}
--- src/main/java/com/genrisk/sistema/interfaces/Query.java
+++
@@ -1,7 +0,0 @@
-package com.genrisk.sistema.interfaces;
-
-import org.springframework.http.ResponseEntity;
-
-public interface Query <I, O>{
-    ResponseEntity<O> execute (I input);
-}
