b90021832d1473ff483d017bf481e44222142c38
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-11-02 12:29:48 -0300

    Conexión con Servidor de MiembroEquipo

---
+++ src/main/java/com/genrisk/sistema/controller/MiembroEquipoController.java
@@ -0,0 +1,62 @@
+package com.genrisk.sistema.controller;
+import com.genrisk.sistema.model.entity.*;
+import com.genrisk.sistema.services.*;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RestController
+@RequestMapping("/miembro-equipo")
+public class MiembroEquipoController {
+    private MiembroEquipoService miembroEquipoService;
+
+    public MiembroEquipoController(MiembroEquipoService miembroEquipoService){
+        this.miembroEquipoService = miembroEquipoService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MiembroEquipo> getMiembroID(@PathVariable Integer id){
+        return miembroEquipoService.getById(id);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<MiembroEquipo>> getUsuarios(){
+        return miembroEquipoService.getAll();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MiembroEquipo> putMiembro(@PathVariable Integer id, @RequestBody MiembroEquipo newMiembro){
+        PutCommand<MiembroEquipo, Integer> miembroNew = new PutCommand<>();
+        miembroNew.setId(id);
+        miembroNew.setNewData(newMiembro);
+        return miembroEquipoService.updatePut(miembroNew);
+    }
+
+    @PostMapping()
+    public ResponseEntity<MiembroEquipo> createMiembro(@RequestBody MiembroEquipo miembro){
+        return miembroEquipoService.createPost(miembro);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMiembro(@PathVariable Integer id){
+        return miembroEquipoService.deleteById(id);
+    }
+}
+    
+
+    
+    
+
+
+
