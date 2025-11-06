fe849237b7dbc83debd09800345d331204e4a438
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-11-06 17:00:45 -0300

    Creación de pacienteDTO para reclutadores

---
+++ src/main/java/com/genrisk/sistema/model/dto/PacReclutadorDTO.java
@@ -0,0 +1,16 @@
+package com.genrisk.sistema.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class PacReclutadorDTO {
+    private String idPaciente;
+    private String tipoPaciente; 
+}
